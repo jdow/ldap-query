b41e7a9b3d7f829c003a4157279f4e62d8fbd7de
Justin Dow <justin@dowhaus.com>
2011-04-17 12:33:45 -0700

    adding comments to explain what's going on

--- ldap-query.py
+++ ldap-query.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 
+# This is my first attempt at writing some python code
+# Hopefully this will start looking better as I clean it up
+
 import sys
 import ldap
 
+# Grab all the variables from ldapconf.py
 from ldapconf import *
 
 def main():
 
+# Currently I only want one argument: the rdn of a user
+# This will get changed in a new version soon
     if (len(sys.argv) > 2):
         print "Please only specify one argument"
         exit()
@@ -16,6 +22,7 @@ def main():
     else:
         keyword = sys.argv[1]
 
+# Initialize the connection to the LDAP server
     try:
         l = ldap.initialize(ldapUri)
         l.simple_bind_s(bindDn, ldapPass)
@@ -27,11 +34,13 @@ def main():
 def my_search(l, keyword):
     scope = ldap.SCOPE_SUBTREE
     filter = rdn + keyword
+# Right now, I just want the sshPublicKey attribute
     retrieve_attributes = ['sshPublicKey']
     count = 0
     result_set = []
     timeout = 0
 
+# use the search method to grab the ssh key's of a given user
     try:
         result_id = l.search(base, scope, filter, retrieve_attributes)
         while 1:
@@ -41,6 +50,8 @@ def my_search(l, keyword):
             else:
                 if result_type == ldap.RES_SEARCH_ENTRY:
                     result_set.append(result_data)
+
+# Step through the resulting tuple and grab all the keys
         if len(result_set) == 0:
             print "No Results."
             return 
@@ -50,7 +61,7 @@ def my_search(l, keyword):
                     key= entry[1]['sshPublicKey']
                     count = count + 1
                     for pubkey in key:
-                        # The following substitutions are a hack to work around "broken" keys in our ldap database
+# The following substitutions are a hack to work around "broken" keys in our ldap database
                         pubkey = pubkey.replace('ssh-rsa\r\n', 'ssh-rsa ') #
                         pubkey = pubkey.replace('ssh-dss\r\n', 'ssh-dss ')
                         pubkey = pubkey.replace('=\r\n', '= ')
